95cb917d0b1e3ca44266c10e4a4b870c2f4a42d7
mac <franquicidad@gmail.com>
2020-04-08 10:26:11 -0500

    pitch perfect first layout

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecording: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record Button was Pressed")
+        
+        recordingLabel.text="Recording in process"
+    }
+    
+    @IBAction func stopRecording(_ sender: UIButton) {
+        recordingLabel.text="Recording Stopped"
+    }
 }
 
